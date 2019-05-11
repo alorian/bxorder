4adc1b1ae88e526533e1158bcdba16afa27ffb66
Alexander Shubin <alorian@yandex.ru>
2019-05-11 23:12:07 +0500

    Расчет стоимости доставок с учетом скидки

--- lib/orderhelper.php
+++ lib/orderhelper.php
@@ -3,17 +3,19 @@
 namespace OpenSource\Order;
 
 use Bitrix\Sale\Delivery;
+use Bitrix\Sale\Delivery\CalculationResult;
 use Bitrix\Sale\Order;
 use Bitrix\Sale\Payment;
 use Bitrix\Sale\PaySystem;
 use Bitrix\Sale\Shipment;
+use Exception;
 
 class OrderHelper
 {
     /**
      * @param Order $order
      * @return Shipment|null
-     * @throws \Exception
+     * @throws Exception
      */
     public static function getFirstNonSystemShipment(Order $order): ?Shipment
     {
@@ -31,7 +33,7 @@ class OrderHelper
      * @param Order $order
      * @return PaySystem\Service[]
      *
-     * @throws \Exception
+     * @throws Exception
      */
     public static function getAvailablePaySystems(Order $order): array
     {
@@ -50,23 +52,39 @@ class OrderHelper
     /**
      * @param Shipment $shipment
      * @param Delivery\Services\Base[] $deliveryObjects
-     * @return null[]|\Bitrix\Sale\Delivery\CalculationResult[]
+     * @return null[]|CalculationResult[]
      *
-     * @throws \Exception
+     * @throws Exception
      */
     public static function calcDeliveries(Shipment $shipment, array $deliveryObjects): array
     {
         $calculatedDeliveries = [];
 
+        $order = $shipment->getParentOrder();
+
         $deliveryId = $shipment->getDeliveryId();
+        $deliveryPrice = $shipment->getField('BASE_PRICE_DELIVERY');
 
         foreach ($deliveryObjects as $obDelivery) {
             $shipment->setField('DELIVERY_ID', $obDelivery->getId());
-            $calculatedDeliveries[$obDelivery->getId()] = $obDelivery->calculate($shipment);
+            $calculationResult = $obDelivery->calculate($shipment);
+
+            if ($calculationResult->isSuccess()) {
+                $shipment->setBasePriceDelivery($calculationResult->getPrice());
+                $arShowPrices = $order->getDiscount()
+                    ->getShowPrices();
+
+                $data = $calculationResult->getData();
+                $data['DISCOUNT_DATA'] = $arShowPrices['DELIVERY'];
+                $calculationResult->setData($data);
+            }
+
+            $calculatedDeliveries[$obDelivery->getId()] = $calculationResult;
         }
 
-        //restore actual delivery id
+        //restore actual data
         $shipment->setField('DELIVERY_ID', $deliveryId);
+        $shipment->setBasePriceDelivery($deliveryPrice);
 
         return $calculatedDeliveries;
     }
