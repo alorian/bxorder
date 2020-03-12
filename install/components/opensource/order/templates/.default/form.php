0c92209a991d57f36f88252833806c4a1529be8e
nikolays93 <nikolays93@ya.ru>
2020-03-12 23:58:27 +0400

    Читабельность кода (Работа над ошибками после обсуждения)

--- install/components/opensource/order/templates/.default/form.php
+++ install/components/opensource/order/templates/.default/form.php
@@ -83,10 +83,17 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
                             <?
                             break;
 
+                        case 'FILE':
+                            ?>
+                            <input id="<?= $arProp['FORM_LABEL'] ?>" type="file"
+                                   name="<?= $arProp['FORM_NAME'] ?>"
+                                   value="<?= $arProp['VALUE'] ?>">
+                            <?
+                            break;
+
                         default:
                             ?>
-                            <input id="<?= $arProp['FORM_LABEL'] ?>"
-                                   type="<?= 'FILE' === $arProp['TYPE'] ? 'file' : 'text' ?>"
+                            <input id="<?= $arProp['FORM_LABEL'] ?>" type="text"
                                    name="<?= $arProp['FORM_NAME'] ?>"
                                    value="<?= $arProp['VALUE'] ?>">
                         <? endswitch; ?>
